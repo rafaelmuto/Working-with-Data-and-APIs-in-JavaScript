e283efb943aee88278135139fdd389e0cc383899
Rafael Muto <r.nagahama@gmail.com>
2019-06-12 00:08:42 -0300

    2.3 HTTP Post Request with fetch()

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+// static server
+app.use(express.static('public'));
+// route+controller
+app.use('/API', (req, res) => {
+  console.log(req.body);
+  res.json({
+    status: 'ok!',
+    latitude: req.body.lat,
+    longitude: req.body.lon
+  });
+});
+
 app.listen(3001, () => {
   console.log('==> running express...');
 });
-
-app.use(express.static('public'));
